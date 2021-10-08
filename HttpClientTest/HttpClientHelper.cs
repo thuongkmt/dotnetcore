395596dff4e603039699812d024d29b9da5e0c78
Thuong (Ebi) <thuongnn@smartosc.com>
2021-10-08 18:06:00 +0700

    ReadAsStreamAsync

--- HttpClientTest/HttpClientHelper.cs
+++ HttpClientTest/HttpClientHelper.cs
@@ -50,7 +50,7 @@ namespace HttpClientTest
 
             using (var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
             {
-                var stream = await response.Content.ReadAsStringAsync();
+                var stream = await response.Content.ReadAsStreamAsync();
 
                 string json = new StreamReader(stream).ReadToEnd();
 
