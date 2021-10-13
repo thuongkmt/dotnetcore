af8fd0d946046173b2ba7bdd7b7320a946a48228
Thuong (Ebi) <thuongnn@smartosc.com>
2021-10-13 11:13:29 +0700

    Add new project for testing Custom Authorize attribute

---
+++ CustomAuthorizeAttribute/FillterAttributes/ApiKeyAuthorizeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAuthorizeAttribute.FillterAttributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ApiKeyAuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        /// <summary>  
+        /// This will Authorize User  
+        /// </summary>  
+        /// <returns></returns>  
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var checkHeader = context.HttpContext.Request.Headers.ContainsKey("HeadLine");
+            if (!checkHeader)
+            {
+                context.Result = new JsonResult("NotAuthorized")
+                {
+                    Value = new
+                    {
+                        Status = "Error",
+                        Message = "Invalid Token"
+                    }
+                };
+            }
+        }
+    }
+}
